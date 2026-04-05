c58b3cd66b4403a3ef0d087f96e342f070b96061
brappo <benjamin.rappo69@gmail.com>
2026-04-06 00:20:45 +0200

    The particles spawn now follow the hand burn

--- Assets/Scripts/BurningWand.cs
+++ Assets/Scripts/BurningWand.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using UnityEngine.VFX;
 
+[RequireComponent(typeof (Renderer))]
+[RequireComponent(typeof (VisualEffect))]
 public class BurningWand : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+	[SerializeField]
+	private float	_dissolveDuration;
+	[SerializeField]
+	private float	_pauseDuration;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private Renderer		_renderer;
+	private	VisualEffect	_vfx;
+
+	private void	Awake()
+	{
+		_renderer = GetComponent<Renderer>();
+		_vfx = GetComponent<VisualEffect>();
+	}
+
+	private void	Update()
+	{
+		float dissolved = 1 - Mathf.Clamp(Time.time % (_dissolveDuration + _pauseDuration) - (_pauseDuration / 2), 0, _dissolveDuration) / _dissolveDuration;
+
+		_renderer.material.SetFloat("_dissolved", dissolved);
+		if (dissolved == 1 || dissolved == 0)
+			_vfx.SetFloat("spawnRate", 0);
+		else
+			_vfx.SetFloat("spawnRate", 1);
+		_vfx.SetFloat("spawnRadius", transform.localScale.x / 2);
+		_vfx.SetFloat("spawnPointY", dissolved * 2 - 1);
+	}
 }
