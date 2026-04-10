f1afd6d9e5e281d0cdc6e98bdc9d5357842e41b2
brappo <benjamin.rappo69@gmail.com>
2026-04-10 19:32:42 +0200

    added some ideas for new vfx : particle edge and particle edge group

--- Assets/Scripts/BurningWand.cs
+++ Assets/Scripts/BurningWand.cs
@@ -6,29 +6,44 @@ using UnityEngine.VFX;
 public class BurningWand : MonoBehaviour
 {
 	[SerializeField]
-	private float	_dissolveDuration;
+	private float			_dissolveDuration;
 	[SerializeField]
-	private float	_pauseDuration;
+	private float			_pauseDuration;
+	[SerializeField]
+	private float			_dissolveParticleRange;
+	[SerializeField, Range(0, 1)]
+	private float			_dissolved;
+	[SerializeField]
+	private bool			_animate;
 
 	private Renderer		_renderer;
 	private	VisualEffect	_vfx;
+	private	Vector2			_defaultSpawnRate;
+	private float			_previousDissolved;
+	private float			_particleSpawnSubtractY;
 
 	private void	Awake()
 	{
 		_renderer = GetComponent<Renderer>();
 		_vfx = GetComponent<VisualEffect>();
+		_defaultSpawnRate = _vfx.GetVector2("spawnRate");
+		_previousDissolved = -1;
+		_particleSpawnSubtractY = _renderer.material.GetFloat("_burnWidth") + _renderer.material.GetFloat("_fireWidth") / 2;
 	}
 
 	private void	Update()
 	{
-		float dissolved = 1 - Mathf.Clamp(Time.time % (_dissolveDuration + _pauseDuration) - (_pauseDuration / 2), 0, _dissolveDuration) / _dissolveDuration;
+		if (_animate)
+			_dissolved = 1 - Mathf.Clamp(Time.time % (_dissolveDuration + _pauseDuration) - (_pauseDuration / 2), 0, _dissolveDuration) / _dissolveDuration;
 
-		_renderer.material.SetFloat("_dissolved", dissolved);
-		if (dissolved == 1 || dissolved == 0)
-			_vfx.SetFloat("spawnRate", 0);
+		if (_dissolved == _previousDissolved)
+			return ;
+		_renderer.material.SetFloat("_dissolved", _dissolved);
+		if (_dissolved < 1 - _dissolveParticleRange || _dissolved > _dissolveParticleRange)
+			_vfx.SetVector2("spawnRate", Vector2.zero);
 		else
-			_vfx.SetFloat("spawnRate", 1);
-		_vfx.SetFloat("spawnRadius", transform.localScale.x / 2);
-		_vfx.SetFloat("spawnPointY", dissolved * 2 - 1);
+			_vfx.SetVector2("spawnRate", _defaultSpawnRate);
+		_vfx.SetFloat("spawnPointY", _dissolved * 2 - 1 - _particleSpawnSubtractY);
+		_previousDissolved = _dissolved;
 	}
 }
